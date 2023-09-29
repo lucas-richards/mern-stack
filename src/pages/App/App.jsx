501df61dfdb814fb47dd71e5d7ca5b2622114264
lucas richards dev <lucasrichardsdev@gmail.com>
2023-09-29 10:21:06 -0700

    part 2

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,32 @@
+import {useState} from "react"
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import NewOrderPage from "../NewOrderPage/NewOrderPage"
+import AuthPage from "../AuthPage/Authpage"
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage"
+import NavBar from "../../components/NavBar/NavBar"
+
+// declare state of user
+
+export default function App() {
+
+
+  const [user,setUser] = useState(null)
+
+  return (
+    <main className="App">
+      { user ?
+      <>
+        <NavBar />
+        <Routes>
+          <Route path="/orders/new" element={<NewOrderPage />} />
+          <Route path="/orders" element={<OrderHistoryPage />} />
+        </Routes>
+      </>
+      :
+      <AuthPage />
+      }
+    </main>
+  );
+}
+
---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,6 @@
+
+export default function App() {
+  return (
+    <h1>NewOrderPage</h1>
+  );
+}
